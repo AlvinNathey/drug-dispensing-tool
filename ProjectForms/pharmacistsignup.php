208bbb33484b9eaa92d64be3d23f993ec7904b14
Anthony Kimanthi 151957 <anthony.muendo@strathmore.edu>
2023-06-08 16:37:35 +0300

    Pharmacist signup added

---
+++ ProjectForms/pharmacistsignup.php
@@ -0,0 +1,24 @@
+<?php 
+
+echo "We're here in the next page.\n";
+require_once("./connection.php");
+
+
+    $pharmacist_id = $_POST["pharmacist_id"];
+    $pharmacist_fname = $_POST["pharmacist_fname"];
+    $pharmacist_lname = $_POST["pharmacist_lname"];
+    $pharm_password = $_POST["pharm_password"];
+       
+
+$sql = "INSERT INTO tblpharmacists (pharmacist_id, pharmacist_fname, pharmacist_lname, pharm_password) 
+VALUES ('$pharmacist_id','$pharmacist_fname', '$pharmacist_lname','$pharm_password')";
+
+if($conn->query($sql) === TRUE) {
+    echo 
+    "<script>alert('Data inserted successfully')</script>";
+}else {
+    echo "Error: ".$sql."<br>".$conn->error;
+}
+
+$conn->close();
+?>
