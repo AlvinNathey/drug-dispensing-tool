d53f31d95d35107977920b96f718b9ccdc257b66
Anthony Kimanthi 151957 <anthony.muendo@strathmore.edu>
2023-07-17 19:56:49 +0300

    changed button links

--- ProjectForms/viewdoctors.php
+++ ProjectForms/viewdoctors.php
@@ -40,7 +40,7 @@
                     <td> $row[doc_lname]</td>
                     <td> $row[doc_password]</td>
                     <td><a href = '/drug-dispensing-tool/drug-dispensing-tool/ProjectForms/editdoctors.php?doc_id=$row[doc_id]'> Edit </a> </td>
-                    <td><a href ='/pdrug-dispensing-tool/drug-dispensing-tool/ProjectForms/deletedoctors.php?doc_id=$row[doc_id]'>Delete</a></td>
+                    <td><a href ='/drug-dispensing-tool/drug-dispensing-tool/ProjectForms/deletedoctors.php?doc_id=$row[doc_id]'>Delete</a></td>
                      </tr>";
                     }
              }
