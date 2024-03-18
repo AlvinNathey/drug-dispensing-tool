1be06648c85a3fd690acfc9103259cdd1799d537
AlvinNathey <alvin.nathey@strathmore.edu>
2024-03-18 16:40:50 +0300

    body complete

--- ProjectForms/body/detailsheader.php
+++ ProjectForms/body/detailsheader.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle;?></title>
-    <link rel="stylesheet" href="drugpage.css">
+    <link rel="stylesheet" href="../drug/drugpage.css">
 </head>
 <body>
     <header class="box0">
       <div id="header-desc">
       <a href="catalog.php?cat=Analgesics">
-    <img class="logo" src="logoremoved.png">
+    <img class="logo" src="../photos/logoremoved.png">
 </a>
             <h1> MyDispenser </h1>   
       </div>       
